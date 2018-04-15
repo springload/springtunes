006c7dac95f3cdd8716d0e6199d318e0c813cf95
Vincent Audebert <vincent.audebert@yahoo.fr>
2018-04-15 18:26:41 +0200

    prop type love

--- client/js/components/App.js
+++ client/js/components/App.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import LastUpdated from '../components/LastUpdated';
 import IntelligentCurrentSong from '../containers/IntelligentCurrentSong';
 import IntelligentControls from '../containers/IntelligentControls';
