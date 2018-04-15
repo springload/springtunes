46dff83542533703440840b2d74ceab0893ddc0d
Vincent Audebert <vincent.audebert@yahoo.fr>
2018-04-15 17:18:02 +0200

    updates tests logo

--- client/js/components/Logo.spec.js
+++ client/js/components/Logo.spec.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Logo from './Logo';
-import ReactTestUtils from 'react-addons-test-utils';
+import renderer from 'react-test-renderer';
 
 describe('Logo component', () => {
     it('renders correctly with min params', () => {
-        const shallowRenderer = ReactTestUtils.createRenderer();
-        const result = shallowRenderer.render(
-            <Logo />
-        );
+        const result = renderer.create(<Logo />).toJSON();
         expect(result).toMatchSnapshot();
     });
 });
